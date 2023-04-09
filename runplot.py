aec1a32405e0bcfe708e90e0162a3f8b9b258864
tyler <18473796+tylerjmitchell@users.noreply.github.com>
2023-04-09 07:32:47 -0400

    Update runplot.py

--- runplot.py
+++ runplot.py
@@ -1,19 +1,22 @@
+import os
 import gpxpy
 import gpxpy.gpx
 import matplotlib.pyplot as plt
 
-# Load GPX file
-gpx_file = open('daily_run.gpx', 'r')
+os.chdir('')
+
+# load GPX file
+gpx_file = open('activity.gpx', 'r')
 gpx = gpxpy.parse(gpx_file)
 
-# Extract track points
+# extract track points
 points = []
 for track in gpx.tracks:
     for segment in track.segments:
         for point in segment.points:
             points.append((point.latitude, point.longitude))
 
-# Plot route
+# plot route
 lats, lons = zip(*points)
 plt.plot(lons, lats)
 plt.xlabel('Longitude')
