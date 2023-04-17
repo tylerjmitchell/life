986116ead96168bd38c06b3988e27bfbec2a2db6
tyler <18473796+tylerjmitchell@users.noreply.github.com>
2023-04-17 12:31:28 -0400

    Update ecoregion.py

--- ecoregion.py
+++ ecoregion.py
@@ -1,11 +1,14 @@
 # import modules and connect to GIS
 from arcgis.gis import GIS
 gis = GIS()
+
 # add map
 map1 = gis.map('Concord, NC, USA')
 map1.basemap = 'osm'
 map1.zoom = 7.5
+
 map1
+
 # get ecoregion layer, select ecoregion level IV labels, lines, polygons (5,6,7), and add visualization
 ecoregion = gis.content.get('3cc13a1b3b8c4f57827f29c09a55b4a9')
 map1.add_layer(ecoregion.layers[5])
