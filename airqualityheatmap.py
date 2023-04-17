5125d804de3e51e844c0f0b6c076b33ae06d2162
tyler <18473796+tylerjmitchell@users.noreply.github.com>
2023-04-17 09:50:47 -0400

    Update airqualityheatmap.py

--- airqualityheatmap.py
+++ airqualityheatmap.py
@@ -13,4 +13,4 @@ airquality = gis.content.get('8dcf5d4e124f480fa8c529fbe25ba04e')
 map1.add_layer(airquality, {'renderer':'HeatmapRenderer',
                             'field':'value',
                             'colors':'jet',
-                            'opactiy:':0.75})
+                            'opacity:':0.75})
